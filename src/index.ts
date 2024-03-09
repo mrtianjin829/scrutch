6ad75d648ccfb82b7ee09c011ac375384a6b1d06
mrtianjin829 <100748047+mrtianjin829@users.noreply.github.com>
2024-03-10 00:07:42 +0530

    Hotfix and implement addBlocks

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import scrutch from "./pjson"
+import { readFileSync } from "fs"
 
-let pjson: scrutch.project = {};
+let pjson = JSON.parse( readFileSync(process.argv[2]).toString() ) as scrutch.iproject;
 
-console.log(JSON.stringify(pjson));
+let project = new scrutch.project(pjson);
+
+project.addBlocks("Sprite1",{
+    bhan: {"opcode": "event_whenflagclicked",                                                      "next": "*#v3wu^;b]AOWUySe?-H",                                                         "parent": null,                                                                         "inputs": {},                                                                           "fields": {},                                                                           "shadow": false,                                                                        "topLevel": true,                                                                       "x": 150,                                                                               "y": 168}
+})
+
+console.log(JSON.stringify(project.json,null,2));
+
--- src/pjson.ts
+++ src/pjson.ts
@@ -1,44 +1,79 @@
+// This is a namespace for the project.json implementation in Scratch.
 namespace scrutch {
+    type blox<T> = { [key: string]: T }
     type codeblock_id = string
 
+    // An functional block in Scratch.
     export interface codeblock {
         opcode: string;
         parent: codeblock_id | null;
         next:   codeblock_id | null;
-        inputs: any;
+        inputs: blox<any[]>;
         fields: any;
         shadow: boolean;
         topLevel: boolean
-    }
 
+        // Extra:
+        x?: number;
+        y?: number;
+    }
+    
+    // Base class for a sprite or a stage
     interface target {
         isStage: boolean;
         name: string;
         variables: any;
         lists: any;
         broadcasts: any;
-        blocks: { [key: codeblock_id]: codeblock };
+        blocks: blox<codeblock>;
         comments: any;
         currentCostume: number;
         costumes: any[];
         sounds: any[];
     }
-
+    
+    // Represents a sprite in scratch.
     export interface sprite extends target {
         isStage: false;
         x: number;
         y: number;
     }
-
+    
+    // Represents a single scratch stage
     export interface stage extends target {
         isStage: true;
     }
+    
+    // Represents a primitive scratch object.
+    type primitive = target | sprite | stage;
 
-    export interface project {
-        targets: target[];
+    export interface iproject {
+        targets: primitive[];
         monitors: any[];
         extensions: any[];
         meta: any;
+    }
+
+    export class project {
+         json: iproject;
+
+         constructor(a: iproject){
+             this.json = a
+         }
+
+         addBlocks(sprite: string, blocks: blox<codeblock>){
+            var foundSprite: sprite | null = null;
+            for(let primit of this.json.targets){
+                if(!primit.isStage && (primit.name == sprite)){
+                    foundSprite = primit as sprite
+                    break;
+                }
+            }
+
+            if(foundSprite == null) throw new Error("Cannot find the sprite specified!");
+
+            Object.assign(foundSprite.blocks,blocks);
+         }
     }
 }
 
